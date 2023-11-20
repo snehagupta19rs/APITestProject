8f243623faf94317f735ae202897b7a78244d3fc
Sneha Gupta <sneha_guptasg@outlook.com>
2023-11-20 12:35:33 +0530

    Added API test project

---
+++ APIDemo/CreateRequest.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIDemo
+{
+    public class CreateRequest
+    {
+        public ListOfUsersDTO GetUsers()
+        {
+            var restClient = new RestClient("https://reqres.in/");
+            var restRequest = new RestRequest("/api/users?page2", Method.Get);
+            restRequest.AddHeader = ("Accept", "application/json");
+        }
+    }
+}
